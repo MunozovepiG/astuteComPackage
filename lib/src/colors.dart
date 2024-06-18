4d6e7c03a1b8986868567fa3e853da110bd52eb4
Muno Gwata <munozovepi@astute.money>
2024-06-18 18:42:41 +0200

    updated the colors

--- lib/src/colors.dart
+++ lib/src/colors.dart
@@ -8,6 +8,7 @@ class AppColors {
   final blue050 = const Color(0xFFF1F6FC);
   final white = const Color(0xffbFFFFFF);
   final blue200 = const Color(0xffbCEF0FF);
+  final bordergrey = const Color(0xFFA09E9D);
   final grey200 = const Color(0xffbE1DFDB);
   final grey400 = const Color(0xffbD8D7C6);
   final grey500 = const Color(0xffbB2B0AE);
@@ -17,8 +18,9 @@ class AppColors {
   final black = const Color(0xffb000000);
   final grey800 = const Color(0xffb1C1B1F);
   final green300 = const Color(0xffbCFF850);
-  final green150 = const Color(0xFFF5FBD6);
 
+  final green150 = const Color(0xFFF5FBD6);
+  final bluegreen200 = const Color(0xCCE7FDF7);
   final green800 = const Color(0xffbb00330F);
   final lavender500 = const Color(0xfbbA386DA);
   final lavender650 = const Color(0xffbCECEFF);
